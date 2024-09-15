1c650052987ce6f54237c3c81aaf587e98ddf911
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-09-15 21:42:06 +0900

    データベースのユニーク制約を削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_15_121151) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_15_124025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_15_121151) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "finished", default: false, null: false
-    t.index ["user_id"], name: "index_walks_on_user_id", unique: true
+    t.index ["user_id"], name: "index_walks_on_user_id"
   end
 
   add_foreign_key "arrivals", "stations"
